440280630b68a72007d24d7b1f00dd7401053d3b
shubhanginagori <45516897+shubhanginagori@users.noreply.github.com>
2021-09-04 00:23:16 +0530

    Add files via upload

---
+++ bubble.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project1
+{
+    class bubble
+    {
+        static void Main(string[] args)
+        {
+
+            int[] intArray = new int[100];
+            Console.WriteLine("Number of elements in the array ?");
+            int x = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the Array Elements : ");
+            for (int i = 0; i < x; i++)
+            {
+                intArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Sorting the array
+            for (int j = 0; j <= x - 2; j++)
+            {
+                //intArray.Length - 2
+                for (int i = 0; i <= x - 2; i++)
+                {
+
+                    if (intArray[i] > intArray[i + 1])
+                    {
+                        int temp = intArray[i + 1];
+                        intArray[i + 1] = intArray[i];
+                        intArray[i] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("After Sorting Array :");
+            for (int item = 0; item <= x - 1; item++)
+            {
+                Console.Write(intArray[item] + " ");
+            }
+
+        }
+    }
+}
